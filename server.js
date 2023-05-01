2347b94c18b796e9e6ef5aaf40194fc2115e0750
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-05-01 14:10:17 +0530

    added html/ejs file and rendered it in server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log("Hello");
+app.set("view engine", "ejs");
 
-    // res.download('server.js');       //when the page loads server.js file gets downloaded
-    res.json({message : "Hello World"});        //to send json object to client
-    // res.status(500).send('Hi');
-    // res.send('Hi');  //we donot use this method generally, it is usually used for testing purposes
+app.get('/', (req, res) => {
+    console.log('app.get executing');
+
+    res.render('index', {text : "World"});
 })
 
 app.listen(3000);
