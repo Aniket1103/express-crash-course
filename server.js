872e7cd1c9131bb4dc1658d51758c27bf0b32a16
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-05-01 16:48:43 +0530

    added users route in server.js

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     console.log('app.get executing');
 
-    res.render('index', {text : "World"});
+    res.render("index", {text : "World"});
+})
+
+app.get("/users", (req, res) => {
+    console.log('This is User list');
+    res.send("User List");
+})
+
+app.get("/users/new", (req, res) => {
+    console.log("User new form")
+    res.send("New user form")
 })
 
 app.listen(3000);
